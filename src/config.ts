9c8b3ed9d100def445798a6d5ffbdcd471bc5a59
unional <homawong@gmail.com>
2017-11-03 00:57:41 -0700

    Update deps.
    Add new version of `color-map` for testing.
    
    SystemJS had problem with last version of `color-map` because the `cjsExportsRegEx` detects its bundled file as `cjs` instead of `global`.
    
    Minifying the bundle file get around that issue.

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export interface DomtureConfig {
   packageManager: 'npm', // | 'jspm', no support for jspm yet.
   /**
    * Specifies the root directory of input files.
+   * This should be a relative path like `./src`
    */
   rootDir: string,
   transpiler: 'none' | 'typescript',
