7eb85f3364d9ca17a4ac77879fbd1c1d62f485c2
unional <homawong@gmail.com>
2018-01-03 00:18:23 -0800

    chore: make wallaby works

--- wallaby.js
+++ wallaby.js
@@ -5,13 +5,16 @@ module.exports = function (wallaby) {
       { pattern: 'fixtures/**', instrument: false },
       "src/**/*.ts",
       "!src/**/*.spec.ts",
-      { pattern: 'node_modules/make-error/**', instrument: false },
-      { pattern: 'node_modules/global-store/**', instrument: false },
-      { pattern: 'node_modules/systemjs-plugin-domture/**', instrument: false },
-      { pattern: 'node_modules/aurelia-logging/**', instrument: false },
       { pattern: 'node_modules/@unional/logging/**', instrument: false },
-      { pattern: 'node_modules/typescript/**', instrument: false },
-      { pattern: 'node_modules/plugin-typescript/**', instrument: false }
+      { pattern: 'node_modules/aurelia-logging/**', instrument: false },
+      { pattern: 'node_modules/color-map/**', instrument: false },
+      { pattern: 'node_modules/global-store/**', instrument: false },
+      { pattern: 'node_modules/istanbul-instrumenter-loader/**', instrument: false },
+      { pattern: 'node_modules/make-error/**', instrument: false },
+      { pattern: 'node_modules/plugin-typescript/**', instrument: false },
+      { pattern: 'node_modules/systemjs-plugin-domture/**', instrument: false },
+      { pattern: 'node_modules/ts-loader/**', instrument: false },
+      { pattern: 'node_modules/typescript/**', instrument: false }
     ],
     "tests": [
       "src/**/*.spec.ts"
