b6f580a4e5140810f839c34ea3cfaacbc0e50df9
Homa Wong <homawong@gmail.com>
2018-02-23 17:50:52 -0800

    chore: update deps

--- src/debug.ts
+++ src/debug.ts
@@ -4,7 +4,7 @@ createDomture({ rootDir: './fixtures/with-subfolder' })
   .then(domture => {
     return domture.import('./index')
       .then(foo => {
-        console.log(foo)
+        console.info(foo)
       })
   })
 .catch(err => err)
